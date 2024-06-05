8e11e045cbef850a61cbdba71da1995e08a02c1d
Poojitha Jayasinghe <indrakeerthi@gmail.com>
2024-06-05 22:01:17 +1000

    main updated

--- main.go
+++ main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/poojitha/yaka-seo/utils"
+	"github.com/poojitha/yaka-seo/webpage"
+	"golang.org/x/net/html"
 )
 
 func init() {
 	utils.LoadEnvs()
 }
 
+func extractLinks(n *html.Node, links chan string) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links <- a.Val
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		extractLinks(c, links)
+	}
+}
+
 func main() {
 
-	utils.LoadUi()
+	url := "https://www.optimumpet.com.au"
+	content, err := webpage.ReadCotent(url)
+	fmt.Println(content)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
