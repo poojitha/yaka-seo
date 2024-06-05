573a9ac822be36167248fb0e4d407f5fb902161a
Poojitha Jayasinghe <indrakeerthi@gmail.com>
2024-06-05 22:01:06 +1000

    read a webpage

---
+++ webpage/ReadCotent.go
@@ -0,0 +1,36 @@
+package webpage
+
+import (
+	"io"
+	"net/http"
+)
+
+func ReadCotent(url string) (string, error) {
+	// Create a new HTTP client
+	client := &http.Client{}
+
+	// Set custom headers to mimic a browser
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.5")
+
+	// Send the request
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	// Return the content as string
+	return string(body), nil
+}
