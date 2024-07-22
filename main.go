74e8ed8aff85fdf3278899c115731835c7a75585
poojitha <indrakeethi@gmail.com>
2024-07-22 21:58:44 +1000

    recursive loop patial

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/poojitha/yaka-seo/utils"
@@ -14,18 +15,34 @@ func init() {
 }
 
 func main() {
-
 	pageContent, err := webpage.ReadCotent("https://www.gossiplanka.com/")
-	var links []string
-
 	if err == nil {
-		links, err = webpage.GetTagValues(pageContent, "a", "href")
+		links, err := webpage.GetTagValues(pageContent, "a", "href")
+		var loopLinks func(links []string)
+
+		loopLinks = func(links []string) {
+			var links2 []string
+			for i := 0; i < len(links); i++ {
+				fmt.Printf("<=====> %s", links[i])
+				pageContent, err := webpage.ReadCotent(links[i])
+				if err == nil {
+					links2, _ = webpage.GetTagValues(pageContent, "a", "href")
+					fmt.Println(links2)
+					loopLinks(links2)
+				}
+				if i == 3 {
+					os.Exit(1)
+				}
+			}
+
+		}
+
+		loopLinks(links)
+
 		if err != nil {
 			fmt.Println("Getting tag values:", err)
 		}
 	}
-
-	fmt.Println(links)
 
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
