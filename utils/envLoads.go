f3120d9c80567a8a8b67e1e7dd3282b40d491970
poojitha <indrakeethi@gmail.com>
2024-08-03 16:46:26 +1000

    hash function refactor

--- utils/envLoads.go
+++ utils/envLoads.go
@@ -24,19 +24,13 @@ Hash generates an SHA-256 hash of the input string and returns the first 'length
 - length: The number of characters to be returned from the beginning of the hashed string.
 - Returns: A string representing the first 'length' characters of the SHA-256 hash of the input string.
 */
-func Hash(str string, length int) string {
-	// Create a new SHA256 hash
-	hash := sha256.New()
 
-	// Write the string to the hash
-	hash.Write([]byte(str))
-
-	// Get the final hashed value as a byte slice
-	hashedBytes := hash.Sum(nil)
-
-	// Encode the byte slice to a hexadecimal string
+func Hash(str string, length int) (string, error) {
+	hasher := sha256.New()
+	if _, err := hasher.Write([]byte(str)); err != nil {
+		return "", err
+	}
+	hashedBytes := hasher.Sum(nil)
 	hashedString := hex.EncodeToString(hashedBytes)
-	returnHash := hashedString[:length]
-
-	return returnHash
+	return hashedString[:length], nil
 }
