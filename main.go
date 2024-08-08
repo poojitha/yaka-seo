d66a6c9e867a384ab00480c0452b7dfc18130e4e
poojitha <indrakeethi@gmail.com>
2024-08-08 22:02:49 +1000

    error checking partially done

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/poojitha/yaka-seo/linkr"
 	"github.com/poojitha/yaka-seo/utils"
 	"github.com/poojitha/yaka-seo/webpage"
 )
@@ -15,7 +17,10 @@ func init() {
 }
 
 func main() {
-	pageContent, err := webpage.ReadCotent("https://www.gossiplanka.com/")
+
+	mainHost, _ := linkr.ParseLink("https://www.gossiplanka.com/fff/kkk")
+	pageContent, err := webpage.ReadCotent(mainHost.Scheme + "://" + mainHost.Host)
+
 	if err == nil {
 		links, err := webpage.GetTagValues(pageContent, "a", "href")
 		var loopLinks func(links []string)
@@ -23,16 +28,19 @@ func main() {
 		loopLinks = func(links []string) {
 			var links2 []string
 			for i := 0; i < len(links); i++ {
-				fmt.Printf("<=====> %s", links[i])
-				pageContent, err := webpage.ReadCotent(links[i])
-				if err == nil {
-					links2, _ = webpage.GetTagValues(pageContent, "a", "href")
-					fmt.Println(links2)
-					loopLinks(links2)
+				if strings.Contains(mainHost.Host, links[i]) {
+					fmt.Printf("Parsed Host: %s", links[i])
+					pageContent, err := webpage.ReadCotent(links[i])
+					if err == nil {
+						links2, _ = webpage.GetTagValues(pageContent, "a", "href")
+						loopLinks(links2)
+					}
+					if i == 10 {
+						os.Exit(1)
+					}
+
 				}
-				if i == 3 {
-					os.Exit(1)
-				}
+
 			}
 
 		}
